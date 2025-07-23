1c136e36b3611ecebdbafa6988a84e58dc9e01c2
shaikh faiz <faizuddinshaikh55@gmail.com>
2025-07-23 13:39:20 +0530

    limit increased

--- server/index.ts
+++ server/index.ts
@@ -23,8 +23,8 @@ app.use(cors({
   credentials: true
 }));
 app.use(morgan('combined'));
-app.use(express.json({ limit: '16mb' }));
-app.use(express.urlencoded({ extended: true, limit: '16mb' }));
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
 // Passport configuration
 configurePassport();
